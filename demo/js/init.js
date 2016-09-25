a659495d34cb00bdfda4c0e5db190d9034e0caf4
Celine Langlade <Cendrillon@MacBook-Pro-de-Celine.local>
2016-09-25 02:01:35 +0200

    ancre de marin

--- demo/js/init.js
+++ demo/js/init.js
@@ -5,3 +5,32 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(function() {
+    var availableTags = [
+    "Céréales",
+    "Oléagineux",
+    "Protéagineux",
+    "Pommes de Terre",
+    "Betteraves",
+    "Legumes",
+    "Fruits",
+    "Cultures Fouragères",
+    "Semis direct vigne",
+    "TCS",
+    "Marraichage",
+    "Agriculture Bio",
+    "Lait",
+    "Bovins",
+    "Porcins",
+    "production Avicole",
+    "Diversification",
+    "Circuit Courts",
+    "Reglementations",
+    "Technologies",
+    "Agroforesterie",
+    ];
+    $("#tags").autocomplete({
+      source: availableTags
+  });
+});
